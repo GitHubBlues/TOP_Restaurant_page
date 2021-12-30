a5b955ccaffb330cae7503678ad3dd9c692b8e46
karima <timmermann@gmx.at>
2021-12-30 20:04:21 +0100

    function for switching between pages

--- src/index.js
+++ src/index.js
@@ -4,27 +4,57 @@ import {makeMenu} from './menu';
 import {makeContact} from './contact';
 
 const mainContainer = document.querySelector('#content');
+const mainFooter = document.querySelector('footer');
 
-
-// /**** Main Page ****/
-// makeHeader();
-// makeBody(mainContainer);
-// let creditOne = "Background image: https://patternico.com";
-// let creditTwo = "Chef image: Alexas_Fotos from Pixabay";
-// makeFooter(creditOne, creditTwo);
-
-
-/**** Menu page ****/
-// makeHeader();
-// makeMenu(mainContainer);
-// let creditOne = "Background image: https://patternico.com";
-// let creditTwo = "";
-// makeFooter(creditOne, creditTwo);
-
-
-/**** Contact page ****/
 makeHeader();
-makeContact(mainContainer);
-let creditOne = "Background image: https://patternico.com";
-let creditTwo = "";
-makeFooter(creditOne, creditTwo);
+const btnHome = document.querySelector('.btn-home');
+const btnMenu = document.querySelector('.btn-menu');
+const btnContact = document.querySelector('.btn-contact');
+const topButtonsContainer = document.querySelector('.top-btn-container');
+
+
+function home(){
+    makeBody(mainContainer);
+    let creditOne = "Background image: https://patternico.com";
+    let creditTwo = "Chef image: Alexas_Fotos from Pixabay";
+    makeFooter(creditOne, creditTwo);
+}
+
+function menu(){
+    makeMenu(mainContainer);
+    let creditOne = "Background image: https://patternico.com";
+    let creditTwo = "";
+    makeFooter(creditOne, creditTwo);
+}
+
+function contact(){
+    makeContact(mainContainer);
+    let creditOne = "Background image: https://patternico.com";
+    let creditTwo = "";
+    makeFooter(creditOne, creditTwo);
+}
+
+function switchPages(e){
+
+    while (mainContainer.firstChild) {
+        mainContainer.firstChild.remove()
+    } 
+    
+    while (mainFooter.firstChild) {
+        mainFooter.firstChild.remove()
+    } 
+
+
+    if (e.target.className == "btn-home"){
+        home()    
+    } else if (e.target.className == "btn-menu"){
+
+        menu()
+    } else if (e.target.className == "btn-contact"){
+        contact()
+    } else{
+    }
+}
+
+topButtonsContainer.addEventListener("click", switchPages);
+home();
