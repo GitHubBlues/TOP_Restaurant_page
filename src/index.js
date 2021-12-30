53f0fd9c3fccbf046da87c2541188ea2e928ae45
karima <timmermann@gmx.at>
2021-12-30 11:44:24 +0100

    main page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,66 @@
 import "./style.css"
+import test from './landing';
 
-alert("Webpack ___ Webpack");
-console.log("Welcome webpackers");
+const mainContainer = document.querySelector('#content');
+
+Main Page
+const mainHeader = document.querySelector('header');
+const mainFooter = document.querySelector('footer');
+
+/* header*/
+const topButtonsContainer = document.createElement('div');
+topButtonsContainer.classList.add("top-btn-container");
+
+const btnHome = document.createElement('button');
+btnHome.textContent = "Home";
+btnHome.classList.add("btn-home");
+
+const btnMenu = document.createElement('button');
+btnMenu.textContent = "Menu";
+btnMenu.classList.add("btn-menu");
+
+const btnContact = document.createElement('button');
+btnContact.textContent = "Contact";
+btnContact.classList.add("btn-contact");
+
+mainHeader.appendChild(topButtonsContainer);
+topButtonsContainer.appendChild(btnHome);
+topButtonsContainer.appendChild(btnMenu);
+topButtonsContainer.appendChild(btnContact);
+
+
+/* body*/
+const mainTitle = document.createElement('h1');
+mainTitle.textContent = "Welcome to Ambrosia's Diner";
+mainTitle.classList.add("main-title");
+
+const mainImage = document.createElement('img');
+mainImage.src = "/img/pizza-g2cb6cf464_1920.png";
+mainImage.classList.add("main-image");
+
+const mainText = document.createElement('div');
+mainText.textContent = "Enjoy our new dishes!";
+mainText.classList.add("main-text");
+
+mainContainer.appendChild(mainTitle);
+mainContainer.appendChild(mainImage);
+mainContainer.appendChild(mainText);
+
+
+/* footer*/
+const footerContainer = document.createElement('div');
+footerContainer .classList.add("footer-container");
+
+const footerOne = document.createElement('button');
+footerOne.textContent = "Background image: https://patternico.com";
+footerOne.classList.add("footer-one");
+
+const footerTwo = document.createElement('button');
+footerTwo.textContent = "Chef image: Alexas_Fotos from Pixabay";
+footerTwo.classList.add("footer-two");
+
+footerContainer.appendChild(footerOne);
+footerContainer.appendChild(footerTwo);
+mainFooter.appendChild(footerContainer);
+
+
---
+++ src/landing.js
@@ -0,0 +1,3 @@
+const test = (name) => 'The name of this restaurant is ' + name;
+
+export default test;
